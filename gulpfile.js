5f209854dbdd7cbe328a9ab737c13e57900c0a85
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-17 18:44:37 -0300

    chore(gulpfile): configuring proxy

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,76 @@
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-plumber = require('gulp-plumber'),
-browserSync = require('browser-sync').create(),
-reload = browserSync.reload,
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-rename = require("gulp-rename");
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  plumber = require("gulp-plumber"),
+  browserSync = require("browser-sync").create(),
+  reload = browserSync.reload,
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename");
 
 // Sass --------------------------------------------
-gulp.task('sass', function() {
-    gulp.src('./css/style.scss')
-      .pipe(plumber({
-        handleError: function (err) {
-          console.log(err)
-          this.emit('end')
+gulp.task("sass", function() {
+  gulp
+    .src("./css/style.scss")
+    .pipe(
+      plumber({
+        handleError: function(err) {
+          console.log(err);
+          this.emit("end");
         }
-      }))
-      .pipe(sass())
-      //.pipe(sass({outputStyle: 'compressed'})) // Minifica o arquivo CSS
-      .pipe(gulp.dest('./'))
-      .pipe(browserSync.reload({
+      })
+    )
+    .pipe(sass())
+    //.pipe(sass({outputStyle: 'compressed'})) // Minifica o arquivo CSS
+    .pipe(gulp.dest("./"))
+    .pipe(
+      browserSync.reload({
         stream: true
-      }))
+      })
+    );
 });
 
 // Server --------------------------------------------
-gulp.task('browserSync', function() {
-    //watch files
-    var files = [
-    './style.css',
-    './*.php',
-    './*.js'
-    ];
+gulp.task("browserSync", function() {
+  //watch files
+  var files = ["./style.css", "./*.php", "./*.js"];
 
-    //initialize browsersync
-    browserSync.init(files, {
+  //initialize browsersync
+  browserSync.init(files, {
     //browsersync with a php server
-    proxy: "localhost/wordpress/",
+    proxy: "localhost/",
     notify: false
-    });
+  });
 });
 
 // Concat > Compress --------------------------------------------
-gulp.task('contat-libs', function() { // Concat
-    gulp.src(['./js/scripts.js', './libs/bootstrap-sass/assets/javascripts/bootstrap.min.js', './libs/jquery/dist/jquery.min.js'])
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('./js/'));
+gulp.task("contat-libs", function() {
+  // Concat
+  gulp
+    .src([
+      "./js/scripts.js",
+      "./libs/bootstrap-sass/assets/javascripts/bootstrap.min.js",
+      "./libs/jquery/dist/jquery.min.js"
+    ])
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("./js/"));
 });
-gulp.task('compress-libs', function () { // Compress
-    gulp.src('js/main.js')
-      .pipe(uglify())
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(gulp.dest('./js/'))
-})
+gulp.task("compress-libs", function() {
+  // Compress
+  gulp
+    .src("js/main.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./js/"));
+});
 
 // GULP TASKS --------------------------------------------
 
 // Gulp Default
-gulp.task('watch', ['browserSync', 'sass'], function(){
-    gulp.watch('./**/*.scss', ['sass']);
-    console.log( 'Success!' );
-})
+gulp.task("watch", ["browserSync", "sass"], function() {
+  gulp.watch("./**/*.scss", ["sass"]);
+  console.log("Success!");
+});
 // Gulp Sass
-gulp.task('default', ['sass', 'contat-libs', 'compress-libs'], function() {
-    console.log( 'Success!' );
+gulp.task("default", ["sass", "contat-libs", "compress-libs"], function() {
+  console.log("Success!");
 });
